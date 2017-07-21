6e97713f6d4bf83c0e9958c49707bd83e700f400
Alister Ferreira <alister.ferreira@infracommerce.com.br>
2017-07-21 18:29:35 -0300

    toolbar added

--- src/app/custom-material-module.ts
+++ src/app/custom-material-module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MdButtonModule, MdCheckboxModule} from '@angular/material';
+import {MdButtonModule, MdCheckboxModule, MdToolbarModule} from '@angular/material';
 
 @NgModule({
-  imports: [MdButtonModule, MdCheckboxModule],
-  exports: [MdButtonModule, MdCheckboxModule],
+  imports: [MdButtonModule, MdCheckboxModule, MdToolbarModule],
+  exports: [MdButtonModule, MdCheckboxModule, MdToolbarModule],
 })
 export class CustomMaterialModule { }
