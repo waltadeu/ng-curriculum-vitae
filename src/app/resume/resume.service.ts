3fe29c1d23c7f0ea3d9afe4ed68bd42229b49942
Álister Lopes Ferreira <alisterlf@gmail.com>
2017-08-03 21:13:43 -0300

    handle error fix

--- src/app/resume/resume.service.ts
+++ src/app/resume/resume.service.ts
@@ -26,8 +26,8 @@ export class ResumeService {
     return body.data || {};
   }
 
-  private handleError(error: any): Promise<any> {
-    console.error('An error occurred', error); // for demo purposes only
-    return Promise.reject(error.message || error);
+  private handleError(error: Response): Observable<any> {
+    console.error(error);
+    return Observable.throw(error.json().error || 'Server error');
   }
 }
