a179c6a06bd175d9aa92380898f87525f0f261c3
Álister Lopes Ferreira <alisterlf@gmail.com>
2017-08-22 00:38:01 -0300

    change collum size

--- src/app/in-mem-resume.service.ts
+++ src/app/in-mem-resume.service.ts
@@ -482,7 +482,7 @@ export class InMemResumeService implements InMemoryDbService {
         }
       }
     ];
-    const resume = resumes[0];
+    const resume = resumes[1];
     return { resume };
   }
 }
